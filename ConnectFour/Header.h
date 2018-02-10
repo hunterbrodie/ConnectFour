cad26e368e97fd17b0227ffaad4775b793748b80
Hunter <Hunter@DESKTOP-PS9M0R8>
2018-02-10 14:09:52 -0500

    i did stuff

--- ConnectFour/Header.h
+++ ConnectFour/Header.h
@@ -1,32 +1,106 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-const int par = 6;
-int* createbrd(std::string board[][par]);
-void printbrd(std::string board[][par]);
+void printbrd(std::vector<std::vector<std::string> > board);
+std::vector<std::vector<std::string> > turn(std::vector<std::vector<std::string> > board, std::string player);
+bool wincheck(std::vector<std::vector<std::string> > board, std::string player);
 
-int* createbrd(std::string board[][par])
-{ 
-	int* pointer = new int;
-	for (int x = 0; x < 7; x++)
-	{
-		for (int y = 0; y < 6; y++)
-		{
-			board[x][y] = "[ ] ";
-		}
-	}
-
-	return pointer;
-}
-
-void printbrd(std::string board[][par])
+void printbrd(std::vector<std::vector<std::string> > board)
 {
-	std::string temp;
-	for (int x = 0; x < 7; x++)
+	for (int x = 5; x >= 0; x--)
 	{
-		for (int y = 0; y < 6; y++)
+		for (int y = 0; y < 7; y++)
 		{
 			 std::cout << board[x][y];
 		}
+		std::cout << "\n";
 	}
+	for (int x = 0; x < 7; x++)
+	{
+		std::cout << " " << x + 1 << "  ";
+	}
+	std::cout << "\n";
 }
+
+ std::vector<std::vector<std::string> > turn(std::vector<std::vector<std::string> > board, std::string player)
+{
+	int per;
+	bool check = false;
+	int col;
+
+	while (check == false)
+	{
+		while (true)
+		{
+			std::cin >> col;
+			if (col > 0 && col < 8)
+			{
+				break;
+			}
+			std::cout << "Please choose a real column.\n";
+		}
+		int tcol = col - 1;
+		for (per = 0; per < 6; per++)
+		{
+			if (board[per][tcol] == "[ ] ")
+			{
+				check = true;
+				break;
+			}
+			else if (per == 5)
+			{
+				std::cout << "Choose another column.\n";
+			}
+		}
+
+		if (check)
+		{
+			board[per][tcol] = player;
+		}
+	}
+	return board;
+
+}
+
+ bool wincheck(std::vector<std::vector<std::string> > board, std::string player)
+ {
+	 bool wincheck = false;
+
+	 for (int x = 0; x < 3; x++)
+	 {
+		 for (int y = 0; y < 7; y++)
+		 {
+			 if (board[x][y] == board[x+1][y] && board[x][y] == board[x+2][y] && board[x][y] == board[x+3][y] && board[x][y] != "[ ] ")
+			 {
+				 wincheck = true;
+			 }
+		 }
+	 }
+	 for (int x = 0; x < 6; x++)
+	 {
+		 for (int y = 0; y < 4; y++)
+		 {
+			 if (board[x][y] == board[x][y + 1] && board[x][y] == board[x][y + 2] && board[x][y] == board[x][y + 3] && board[x][y] != "[ ] ")
+			 {
+				 wincheck = true;
+			 }
+		 }
+	 }
+	 for (int x = 0; x < 2; x++)
+	 {
+		 for (int y = 0; y < 3; y++)
+		 {
+			 if (board[x][y] == board[x + 1][y + 1] && board[x][y] == board[x + 2][y + 2] && board[x][y] == board[x + 3][y + 3] && board[x][y] != "[ ] ")
+			 {
+				 wincheck = true;
+			 }
+		 }
+	 }
+
+	 if (wincheck == true)
+	 {
+		 std::cout << player << "won.\n";
+	 }
+	 return wincheck;
+ }
--- ConnectFour/Source.cpp
+++ ConnectFour/Source.cpp
@@ -2,6 +2,31 @@
 
 int main()
 {
-	std::string brd[7][par];
-	brd = createbrd(brd);
+	bool win = false;
+	std::string r = "[R] ";
+	std::string y = "[Y] ";
+	std::vector<std::string> board1(7,"[ ] ");
+	std::vector<std::vector<std::string> > board(6, board1);
+	printbrd(board);
+
+	while (true)
+	{
+		board = turn(board, r);
+		printbrd(board);
+		win = wincheck(board, r);
+		if (win)
+		{
+			break;
+		}
+		board = turn(board, y);
+		printbrd(board);
+		win = wincheck(board, y);
+		if (win)
+		{
+			break;
+		}
+	}
+
+	system("pause");
+	return 0;
 }
